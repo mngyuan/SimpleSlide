c99846a598af2af0ee58b60da96fee8ace3137cb
birdway <xinghuadou@berkeley.edu>
2014-12-14 01:23:31 -0800

    working browser implementation, moved files around

---
+++ interpreter.js
@@ -0,0 +1,141 @@
+// factory for making tag functions that want to map evalNode over their children
+function makeEvalTag(fn) {
+  return function(node) {
+    var children = node.children.map(evalNode);
+    return fn(node, children);
+  }
+}
+
+var defaultEvalTag = makeEvalTag(function(node, children) { 
+  // default just wraps children and gives the tag name as css class name
+  var domNode = document.createElement(node.multline ? 'div' : 'span');
+  domNode.className = node.tag;
+  children.forEach(function (child) {
+    domNode.appendChild(child);
+  });
+  return domNode;
+});
+
+// numbered fragments are tags with just a number in them like _1 test _
+// they are revealed in increasing order, after unumbered fragments
+function evalNumberedFragment(node) {
+  var domNode = defaultEvalTag(node);
+  domNode.type = domNode.className = 'numberedFragment'; 
+  domNode.number = node.tag;
+  return domNode;
+}
+
+function evalFragment(node) {
+  var domNode = defaultEvalTag(node);
+  domNode.type = domNode.className = 'fragment';
+  return domNode;
+}
+
+var tags = [ { tag: /^[0-9]+$/, function: evalNumberedFragment} ,
+             { tag: /^f$/, function: evalFragment } ];
+
+function findEvalTag(string) { // look for the function to eval a tag node
+  var fn;
+  tags.forEach(function(tag) {
+    if (string.match(tag.tag)) {
+      fn = tag.function;
+    }
+  });
+  return fn || defaultEvalTag;
+}
+
+
+function evalNode(node) {
+  if (node.type === 'tag') {
+    return findEvalTag(node.tag)(node);
+  } else if (node.type === 'text') {
+    var domNode = document.createElement('span');
+    domNode.textContent = domNode.innerText = node.text;
+    return domNode;
+  } else if (node.type === 'line') {
+    var domNode = document.createElement('div');
+    node.children.map(evalNode).forEach(function (child) {
+      domNode.appendChild(child);
+    });
+    return domNode;
+  } else if (node.type === 'ul') {
+    var domNode = document.createElement('ul');
+    domNode.style.margin = '0px ' + node.level*10 + 'px';
+    node.children.map(evalNode).forEach(function (child) {
+      var li = document.createElement('li');
+      li.appendChild(child);
+      domNode.appendChild(li);
+    });
+    return domNode;
+  }
+}
+
+function makePage(page) {
+  var domPage = document.createElement('div');
+  var header = document.createElement('h1');
+  header.appendChild(evalNode(page.title));
+  [header].concat(page.children.map(evalNode)).forEach(function (child) {
+    domPage.appendChild(child);
+  });
+  var numberedFragmentsCol = domPage.getElementsByClassName('numberedFragment');
+  var numberedFragments = [].slice.call(numberedFragmentsCol)
+                                 .sort(function (a, b) {
+                                   return a.number - b.number;
+                                 });
+  var fragmentsCol = domPage.getElementsByClassName('fragment');
+  domPage.fragments = [].slice.call(fragmentsCol)
+                         .concat(numberedFragments);
+  domPage.index = 0; // next fragment to reveal
+  return domPage;
+}
+
+var i = 0;
+
+function next() {
+  var page = pages[i];
+  if (page.index < page.fragments.length) {
+    var fragment = page.fragments[page.index];
+    fragment.className = 'revealed' + fragment.type;
+    page.index++;
+  } else if (i < pages.length - 1) {
+    document.body.removeChild(pages[i]);
+    i++;
+    document.body.appendChild(pages[i]);
+  }
+}
+
+function prev() {
+  var page = pages[i];
+  if (page.index > 0) {
+    var fragment = page.fragments[page.index - 1];
+    fragment.className = fragment.type;
+    page.index--;
+  } else if (i > 0) {
+    document.body.removeChild(pages[i]);
+    i--;
+    document.body.appendChild(pages[i]);
+  }
+}
+
+function makeSlideShow(input) {
+  var ast = parse(input);
+  pages = ast.map(makePage);
+  console.log(pages);
+  document.body.appendChild(pages[0]);
+  document.getElementById('next').onclick = next;
+  document.getElementById('prev').onclick = prev;
+}
+
+window.onload = function() {
+  document.getElementById('input').addEventListener('change', function(evt) {
+    var f = evt.target.files[0];
+    if (f) {
+      var r = new FileReader();
+      r.onload = function(e) { 
+	      var contents = e.target.result;
+        makeSlideShow(contents);
+      }
+      r.readAsText(f);;
+    }
+  }, false);
+};
---
+++ parser.js
@@ -0,0 +1,168 @@
+function parse(text) {
+  var pages = [];
+  var nodes = [];
+  var lines = text.split('\n');
+  var globalDeclarations = [];
+  var globalDec = /^:\w+/; // regex for global declarations ie :background blue
+  var multTag = /^_\w+$/; // regex for a multi-line tag ie: _code
+  var closeTag = /^_\s*$/; //regex for closing a multi-line tag
+  var ul = /^[\t ]*\*/; //regex for unordered list item ie *item1
+  lines.unshift(''); // add a blank line in case of no title first page
+
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (lines[i+1] && lines[i+1] === '====') {
+      while (nodes.length) {
+        var node = nodes.pop();
+        console.log('Warning: unclosed tag: ' + node.tag + ' closed automatically.');
+        node.text = lines.slice(node.start, i).join('\n');
+        pushNode(node);
+      }
+      if (pages.length != 0) { // if there is a previous page
+        var prevPage = pages[pages.length - 1];
+        prevPage.text = lines.slice(prevPage.start, i).join('\n'); // fill out the text field
+      }
+      var title = parseLine(line);
+      pages.push(newPage(title, globalDeclarations, i));
+      currNode = pages[pages.length - 1];
+      i++;
+    } else if (line.match(globalDec)) {
+      pushDec(parseDec(line));
+    } else if (line.match(multTag)) {
+      nodes.push(newNode(line.substring(1), i));
+    } else if (line.match(closeTag)) { // if the line is closing a multi-line tag
+      if (nodes.length) {
+        var node = nodes.pop(); // fill out the text field of the node
+        node.text = lines.slice(node.start, i + 1).join('\n');
+        pushNode(node);
+      } else {
+        console.log('Warning: ignoring unmatched close tag');
+      }
+    } else if (line.match(ul)) {
+      pushNode(parseUL(line));
+    } else if (line != '') {
+      pushNode(parseLine(line));
+    }
+  }
+
+  var prevPage = pages[pages.length - 1]; // fill out text field for last page
+  prevPage.text = lines.slice(prevPage.start, i).join('\n'); 
+  while (nodes.length) { // close all tags on last page
+    var node = nodes.pop();
+    console.log('Warning: unclosed tag: ' + node.tag + ' closed automatically.');
+    node.text = lines.slice(node.start, i).join('\n');
+    pushNode(node);
+  }
+
+
+  function newNode(tag, start) {
+    return { type:'tag', tag: tag, start: start, children: [], multiline: true };
+  }
+
+  function pushNode(node) {
+    if (nodes.length == 0) { // there are no more parent nodes
+      if (pages.length) {
+        pages[pages.length - 1].children.push(node); // push directly to the current page
+      } else {
+        console.log('Warning, declarations outside of the first page ignored');
+      }
+    } else {
+      nodes[nodes.length - 1].children.push(node); // push to the node above
+    }
+  }
+
+  function newPage(title, declarations, start) {
+    var page = { title: title, start: start, properties: [], children: [] };
+    declarations.forEach(function (v) { // deep copy current declaration list
+      page.properties.push(v);
+    });
+    return page;
+  }
+
+  function parseDec(line) {
+    var dec = line.substring(1).split(/\s/); // split by whitespace character
+    return { key: dec[0], values: dec.slice(1) };
+  }
+
+  function pushDec(dec) {
+    if (dec.values.length) { // if there are values to be assigned, open a new declaration
+      globalDeclarations.push(dec);
+    } else { // otherwise close the last open declaration of the same key
+      var index = -1;
+      for (var i = globalDeclarations.length - 1; i >= 0; i--) {
+        if (globalDeclarations[i].key === dec.key) {
+          index = i;
+          break;
+        }
+      }
+      if (index != -1) {
+        globalDeclarations.splice(index, 1);
+      } else {
+        console.log('Warning: trying to close unopened declaration: ' + dec.key);
+      }
+    }
+  }
+
+  function parseUL(line) {
+    var indent = line.indexOf('*');
+    var node = { type: 'ul', level: indent }; // how many indentation levels deep
+    node.children = [parseLine(line.substring(indent + 1))];
+    return node;
+  }
+
+  function parseLine(line) { // pretty much the same structure as parseText, except by line
+    var newChildren = [];
+    var newNode = { type: 'line', children: newChildren };
+    var nodes = [];
+    var currentText = '';
+    var open = /_\w+/; // regex for open tag
+    var close = /_/; // regex for close tag
+    var words = line.split(/\s/);
+    for (var i = 0; i < words.length; i++) {
+      var word = words[i];
+      if (word.match(open)) {
+        if (currentText) {
+          pushNode( { type: 'text', text: currentText } );
+          currentText = '';
+        }
+        nodes.push( { type: 'tag', tag: word.substring(1), start: i, children: [],
+                      multiline: false });
+      } else if (word.match(close)) {
+        if (nodes.length) {
+          var node = nodes.pop();
+          node.children.push({ type: 'text', text: currentText });
+          node.text = words.slice(node.start, i).join(' ');
+          pushNode(node);
+          currentText = '';
+        } else {
+          console.log('Warning: ignoring unmatched close tag');
+        }
+      } else {
+        currentText += word + ' ';
+      }
+    }
+
+    if (currentText) {
+      pushNode( { type: 'text', text: currentText });
+    }
+
+    while (nodes.length) {
+      var node = nodes.pop();
+      console.log('Warning: unclosed tag: ' + node.tag + ' closed automatically.');
+      node.text = words.slice(node.start, i).join(' ');
+      pushNode(node);
+    }
+
+    function pushNode(node) {
+      if (nodes.length == 0) {
+        newChildren.push(node);
+      } else {
+        nodes[nodes.length - 1].children.push(node); 
+      }
+    }
+    return newNode;
+
+  }
+
+  return pages;
+}
